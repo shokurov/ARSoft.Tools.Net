3b31d0f85ea7f7dbccfe7d416d59bb8f78b145cb
Egor Shokurov <egors@rapidsoft.ru>
2018-02-04 22:09:15 +0300

    Added simple test for email validation

---
+++ ARSoft.Tools.Net.Tests/MXLookupTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net.Dns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ARSoft.Tools.Net.Tests
+{
+    [TestClass]
+    public class MxLookupTest
+    {
+        [TestMethod]
+        public void CanValidateEmailsTest()
+        {
+            var emails = new List<string>();
+
+            using (var sr = new StreamReader(new GZipStream(
+                GetType().Assembly
+                    .GetManifestResourceStream(
+                        $"{GetType().Assembly.FullName.Split(',').First()}.TestData.MOCK_DATA.csv.gz") ??
+                throw new InvalidOperationException(), CompressionMode.Decompress)))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    emails.Add(line);
+                }
+            }
+
+            var domains = emails.Select(email => email.Substring(email.IndexOf('@') + 1)).Distinct().ToArray();
+
+            var validDomains = new HashSet<string>();
+            var checker =
+                new DnsClient(DnsClient.GetLocalConfiguredDnsServers(), 60);
+
+            var aTask = Task.WhenAll(domains.Select(d => checker.ResolveAsync(DomainName.Parse(d))));
+            var mxTask =
+                Task.WhenAll(domains.Select(d => checker.ResolveAsync(DomainName.Parse(d), RecordType.Mx)));
+
+            Task.WaitAll(mxTask, aTask);
+
+            validDomains.UnionWith(mxTask.Result.Select(mx =>
+                mx?.Questions.First().Name.ToString()));
+
+            validDomains.UnionWith(aTask.Result.Select(a =>
+                a?.Questions.First().Name.ToString()));
+
+            Assert.AreEqual(435, validDomains.Count);
+        }
+    }
+}
---
+++ ARSoft.Tools.Net.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ARSoft.Tools.Net.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ARSoft.Tools.Net.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27226ced-84bd-4be2-9803-ec80dc91495b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
